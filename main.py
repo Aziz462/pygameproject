8c6d5d09f5e1e7fbf3862daaf8eb5c97374cf6d6
Aziz Bakhovaddinov <bakhovaddinov200612@yandex.ru>
2021-01-19 21:51:16 +0300

    changed assets and improved running animation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, os, sys
+import pygame, os, sys 
 
 
 FPS = 60
@@ -30,11 +30,12 @@ def load_image(name, colorkey=None):
 class Player(pygame.sprite.Sprite):
     def __init__(self, idle_sheet, run_sheet, columns, rows, x, y):
         super().__init__(all_sprites)
+        self.is_left = False
         self.idle_frames = []
         self.cut_sheet(idle_sheet, columns, rows, self.idle_frames)
         self.cur_frame = 0
         self.run_frames = []
-        self.cut_sheet(run_sheet, columns, rows, self.run_frames)
+        self.cut_sheet(run_sheet, 6, rows, self.run_frames)
         self.image = self.idle_frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
         self.cur_state = 'idle'
@@ -51,18 +52,21 @@ class Player(pygame.sprite.Sprite):
             self.image = self.idle_frames[self.cur_frame]
         else:
             self.image = self.run_frames[self.cur_frame]
-        self.bigger_img = pygame.transform.scale(self.image, (50, 50))
-        self.image = self.bigger_img
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        if self.is_left:
+            self.image = pygame.transform.flip(self.image, True, False)
+
+        
+        
     
     def move(self, x, y):
         self.rect = self.rect.move(x, y)
         self.image = self.run_frames[self.cur_frame]
         self.cur_state = 'run'
-        self.bigger_img = pygame.transform.scale(self.image, (50, 50))
-        self.image = self.bigger_img
+        self.update()
 
 
-player = Player(load_image('knight_idle.png'), load_image('knight_run.png'), 4, 1, 50, 50)
+player = Player(load_image('idle_hero.png'), load_image('run_hero.png'), 4, 1, 50, 50)
 
 z = 0
 
@@ -79,20 +83,24 @@ while True:
                event.key == pygame.K_LEFT:
                 player.cur_state = 'idle'
     
-    if z % 10 == 0:
+    if z % 6 == 0:
         player.cur_frame = (player.cur_frame + 1) % len(player.idle_frames)
     # if keys[pygame.K_LEFT]:
     keys = pygame.key.get_pressed()
     x = (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * 5
     y = (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * 5
     if x:
+        print(x)
+        if x < 0:
+            player.is_left = True
+        elif x > 0:
+            player.is_left = False
         player.move(x, 0)
     if y:
         player.move(0, y)
     # player.rect.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * 10
     # player.rect.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * 10
 
-    print(player.cur_state)
     screen.fill(pygame.Color("black"))
     all_sprites.draw(screen)
     all_sprites.update()
