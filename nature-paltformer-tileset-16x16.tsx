051b40b4396bb727c2154a8b00a65ae6894802b8
Aziz Bakhovaddinov <bakhovaddinov200612@yandex.ru>
2021-03-01 18:19:07 +0300

    added tilesets

---
+++ nature-paltformer-tileset-16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="nature-paltformer-tileset-16x16" tilewidth="15" tileheight="15" spacing="1" tilecount="77" columns="7">
+ <image source="assets/nature-paltformer-tileset-16x16.png" width="112" height="176"/>
+</tileset>
